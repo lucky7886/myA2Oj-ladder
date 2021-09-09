b99cd68f982c45180ab0aaee67493a147db5b4fa
unknown <luckytiwari7886@gmail.com git config --global user.email luckytiwari7886@gmail.comgit config --global user.name lucky7886>
2021-09-09 12:43:17 +0530

    added how to kill port

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ import chalk from 'chalk'
 import dotenv from 'dotenv'
 
 dotenv.config()
+/*
+ to kill port
+  netstat -ano | findstr 8000
+   taskkill /F /PID 8436 (processId)
+
+ */
 
 const SUCCESS = chalk.bold
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-  console.log(SUCCESS('hey i m listingi on 8000'))
+  console.log(SUCCESS('hey i m listing on 8000'))
 })
